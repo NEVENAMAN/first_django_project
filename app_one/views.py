96dfb7e35a2a63e49a37277a5d05c1e3965a51bd
Neven Amen <nevenamen2000@gmail.com>
2022-12-07 13:08:30 +0200

    first django project

---
+++ app_one/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,redirect,HttpResponse
+from django.http import JsonResponse
+
+def root(request):
+    return redirect("/blogs")
+
+def index(request):
+    return render(request,"show.html")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect("/")
+
+def show(request,number):
+    return HttpResponse("placeholder to display blog number: " + number )
+
+def edit(request,number):
+    return HttpResponse("placeholder to edit blog " + number)
+
+def destroy(request,number):
+    return redirect("/")
+
+def jsonmethod(request):
+    context = {
+        'title' : 'My first blog',
+        'content': '  Lorm in blog Lorm in blog Lorm in blog Lorm in blog'
+    }
+    return JsonResponse(context)
